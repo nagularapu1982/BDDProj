5edd24d5c4f0e1ed10c55a825d1b063ee4463d73
Nagularapu <naga.nagularapu@accenture.com>
2019-09-27 15:44:57 +0100

    Include the driver and add the time stamp to report

--- src/test/java/Runners/TestRunner.java
+++ src/test/java/Runners/TestRunner.java
@@ -1,24 +1,38 @@
 package Runners;
+import com.cucumber.listener.ExtentProperties;
 import com.cucumber.listener.Reporter;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+
         features = "src/test/resources/Features",
         glue= {"Steps"},
-        plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"},
+       plugin = { "com.cucumber.listener.ExtentCucumberFormatter:"},
+//        plugin = {"com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:"},
+//        @CucumberOptions( plugin = { "com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:path/report.html"} )
         monochrome = true
 
 )
 
 public class TestRunner {
+    static String ReportName="NNR";
+    static String logFileName = new SimpleDateFormat("yyyyMMddHHmm'.txt'").format(new Date());
+    @BeforeClass
+    public static void setup() {
+        ExtentProperties extentProperties = ExtentProperties.INSTANCE;
+        extentProperties.setReportPath("target/cucumber-reports/Results"+logFileName+".html"); //used same string name to create the file with the same name.
+    }
     @AfterClass
     public static void writeExtentReport() {
-        Reporter.loadXMLConfig("C:\\AutomationPratice\\BDDProj\\src\\test\\resources\\extent-config.xml");
+        Reporter.loadXMLConfig("src/test/resources//extent-config.xml");
     }
 }
--- src/test/java/Steps/Hooks.java
+++ src/test/java/Steps/Hooks.java
@@ -12,12 +12,21 @@ public class Hooks {
     WebDriver driver;
     @Before
     public void BeforeSteps() {
-        System.out.println("Beofre the scenario");
+        System.out.println("Browser opem");
+        System.setProperty("webdriver.chrome.driver","src/test/resources/Drivers/chromedriver-32.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://www.shop.demoqa.com");
+
     }
 
     @After
-    public void AfterSteps() {
-//        driver.quit();
+
+    public void AfterSteps() throws InterruptedException {
+
+        Thread.sleep(4000);
+        driver.quit();
 //        driver.close();
         System.out.println("after scenario");
 
